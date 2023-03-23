cae7fad91f61937e591a53a7ba8f8b028c11b901
Tony Falcon <tonyfalcondevweb@gmail.com>
2023-03-23 15:26:58 +0100

    vercel 2

--- src/apiRiot/apiRiot.js
+++ src/apiRiot/apiRiot.js
@@ -3,13 +3,12 @@ import axios from "axios";
 export const url = "/api/"
 export const urlMastery = "/mastery/"
 
-const apiValue = import.meta.env.VITE_REACT_APP_RIOT_TOKEN_VALUE;
+const apiValue = import.meta.env.VITE_VERCEL_ENV_RIOT;
 
 export const summonerRequest = (inputs) => {
     const config = { headers : {        
         "X-Riot-Token" : apiValue
     } };
-    console.log(config);
     return axios.get( url + inputs, config);
 }
 
@@ -17,6 +16,5 @@ export const masteryRequest = (inputs) => {
     const config = { headers : {        
         "X-Riot-Token" : apiValue
     } };
-    console.log(config);
     return axios.get( urlMastery + inputs + "/top?count=5", config);
 }
