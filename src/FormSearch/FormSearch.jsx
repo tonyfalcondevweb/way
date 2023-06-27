18428590b1c8730e17145ce15c498addd28d8018
Tony Falcon <tonyfalcondevweb@gmail.com>
2023-06-27 16:15:40 +0200

    form2

--- src/FormSearch/FormSearch.jsx
+++ src/FormSearch/FormSearch.jsx
@@ -107,10 +107,10 @@ const FormSearch = ({ affichageComposant, setAffichageComposant, nomInvocateur, 
                         <li><div className="dropdown-item" onClick={handleClickClassementChampion}  >Classement</div></li>
                     </ul>
                 </div>
-                <label htmlFor="invocateur">invocateur</label >
+                <label htmlFor="invocateur"></label >
+                <button onClick={handleClickDuo} type="submit" className="btn btn-formSearch rounded-start-0 rounded-end-pill">WAY?</button>
                 <input id='invocateur' className='form-control rounded-end-0 rounded-start-0 input-formSearch' value={nomInvocateur} onChange={handleChangenomInvocateur} type="text" placeholder="Nom D'invocateur" />
             </form>
-                <button onClick={handleClickDuo} type="submit" className="btn btn-formSearch rounded-start-0 rounded-end-pill">WAY?</button>
 
 
         </div>
